0ed368557321d09cee2f74f7139bd10d13cd1bfa
Harshal Limaye <harshal.limaye@outlook.com>
2020-09-12 14:19:13 +0530

    05. Code Splitting

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,11 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    index: './src/index.js'
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
-  plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new HtmlWebpackPlugin({
-      title: 'Development'
-    })
-  ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
   }
 }
